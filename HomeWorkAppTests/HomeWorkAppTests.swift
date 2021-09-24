9ce3de853d285314050d1274326995600c411149
Gilson Santos <gilsonsantosti@gmail.com>
2021-09-24 16:18:58 -0300

    teste

--- HomeWorkAppTests/HomeWorkAppTests.swift
+++ HomeWorkAppTests/HomeWorkAppTests.swift
@@ -19,6 +19,7 @@ class HomeWorkAppTests: XCTestCase {
 
     func testExample() throws {
         XCTAssertFalse(true)
+        XCTAssertFalse(true)
     }
 
     func testPerformanceExample() throws {

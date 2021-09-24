cd84a6b6d7870706afae66e34c6d4e215f16772f
Gilson Santos <gilsonsantosti@gmail.com>
2021-09-24 18:07:22 -0300

    ajustes

--- fastlane/Tests/Tests.rb
+++ fastlane/Tests/Tests.rb
@@ -6,7 +6,7 @@ desc "Lane to run all the main app tests"
 lane :tests do
     scan(devices: devices,
          formatter: "xcpretty-json-formatter",
-         scheme: "HomeWorkApp-Mock",
+         scheme: "HomeWorkApp",
          clean: true,
          code_coverage: true,
          buildlog_path: "./build/tests_log")
